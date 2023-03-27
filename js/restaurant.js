cddc457324e412b94c7dec2793a2c30a9e63c629
Tadeo <7ade4@gmail.com>
2023-03-27 12:40:12 -0300

    Terminado

--- js/restaurant.js
+++ js/restaurant.js
@@ -8,19 +8,10 @@ var Restaurant = function(id, nombre, rubro, ubicacion, horarios, imagen, califi
     this.calificaciones = calificaciones;
 }
 
-/* Restaurant.prototype.reservarHorario = function(horarioReservado) {
-    for (var i = 0; i < this.horarios.length; i++) {
-        if (this.horarios[i] === horarioReservado) {
-            this.horarios.splice(i, 1);
-            return;
-        }
-    }
-} */
-
 Restaurant.prototype.reservarHorario =  function (horarioReservado){
     var horariosDisponibles = this.horarios.filter(horario => horarioReservado !== horario);
     this.horarios = horariosDisponibles;
-     }
+}
      
 Restaurant.prototype.calificar = function(nuevaCalificacion) {
     if (Number.isInteger(nuevaCalificacion) && nuevaCalificacion > 0 && nuevaCalificacion < 10) {
@@ -36,20 +27,6 @@ function promedio (calificaciones){
    var promedio = sumatoria(calificaciones)/calificaciones.length;
    return Math.round(promedio * 10) / 10;
 }
-
-/* Restaurant.prototype.obtenerPuntuacion = function() {
-    if (this.calificaciones.length === 0) {
-        return 0;
-    } else {
-        var sumatoria = 0;
-        for (var i = 0; i < this.calificaciones.length; i++) {
-            sumatoria += this.calificaciones[i]
-        }
-        var promedio = sumatoria / this.calificaciones.length;
-        return Math.round(promedio * 10) / 10;
-    }
-
-} */
 
 Restaurant.prototype.obtenerPuntuacion = function () {
     if (this.calificaciones.length === 0) {
